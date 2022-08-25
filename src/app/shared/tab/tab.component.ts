ee23379459d0597fe63155f3b6cad11fa3703491
Bram van Rijswijk <b.vanrijswijk@outlook.com>
2022-08-25 10:28:35 +0200

    Dynamically show tab content

---
+++ src/app/shared/tab/tab.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.css']
+})
+export class TabComponent implements OnInit {
+
+  @Input() title: string = ''
+  @Input() isActive = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
