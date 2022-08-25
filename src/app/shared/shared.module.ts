0490fb20aa0e41778c89288af1412a3a0d28282a
Bram van Rijswijk <b.vanrijswijk@outlook.com>
2022-08-25 09:41:12 +0200

    Setup modal and ModalService

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ModalComponent} from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+
+// import {ModalService} from "../services/modal.service";
+
+@NgModule({
+    declarations: [
+        ModalComponent,
+        TabsContainerComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        ModalComponent
+    ],
+    // providers: [
+    //     ModalService
+    // ]
+})
+export class SharedModule {
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthModalComponent} from './auth-modal/auth-modal.component';
 
+import {SharedModule} from "../shared/shared.module";
+
 
 @NgModule({
     declarations: [
@@ -11,7 +13,8 @@ import {AuthModalComponent} from './auth-modal/auth-modal.component';
         AuthModalComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        SharedModule
     ]
 })
 export class UserModule {
